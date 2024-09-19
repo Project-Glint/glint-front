cfaa8f968bc2f75074580a63fdcf007b036f8d97
dlskfo306 <dlskfo306@gmail.com>
2024-09-19 16:57:59 +0900

    feat: storybook 예시 코드 작성 (기존 버튼 컴포넌트) (#2)

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { buttonVariant } from "types";
+import * as S from "./Button.styled";
+
+interface ButtonProps {
+  className?: string;
+  children?: React.ReactNode;
+  variant?: buttonVariant;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({
+  className,
+  children,
+  variant,
+  disabled,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <S.Button
+      className={className}
+      variant={variant}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </S.Button>
+  );
+};
+
+export default Button;
---
+++ stories/Button.stories.ts
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "components";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: [
+        "xsPink",
+        "xsWhite",
+        "smPink",
+        "mdPink",
+        "lgPink",
+        "mdWhite",
+        "underline",
+        "icon",
+      ],
+    },
+    disabled: { control: "boolean" },
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: "Button",
+    variant: "mdPink",
+    disabled: false,
+  },
+};
+
+export const XsPink: Story = {
+  args: {
+    children: "Xs Pink Button",
+    variant: "xsPink",
+    disabled: false,
+  },
+};
+
+export const Underline: Story = {
+  args: {
+    children: "Underline Button",
+    variant: "underline",
+    disabled: false,
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    children: "Icon Button",
+    variant: "icon",
+    disabled: false,
+  },
+};
