4d1a1b47b4f27f58fba07aec565def0d4634d683
dlskfo306 <dlskfo306@gmail.com>
2024-10-25 20:17:44 +0900

    chore: app/page 예시코드 삭제 (#16)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,13 @@
 'use client';
 
-import { ImageUpload, RadioButton, Select } from 'components';
-import { useForm } from 'react-hook-form';
-
 /**
  * App HOME
  * @return {JSX}
  */
 export default function Home() {
-  const { control } = useForm({
-    defaultValues: { gender: 'male' },
-  });
-  const radioList = [
-    { key: 'male', label: '남자' },
-    { key: 'female', label: '여자' },
-  ];
-
-  const selectList = [
-    { key: 'male', label: '남자' },
-    { key: 'female', label: '여자' },
-  ];
-
-  const handleImageChange = (file: File | null) => {
-    if (file) {
-      console.log('upload file:', file);
-    } else {
-      console.log('delete image');
-    }
-  };
   return (
     <div>
       <h1>Home</h1>
-      <RadioButton control={control} radioList={radioList} name="gender" />
-      <div style={{ marginLeft: '100px' }}>
-        <Select
-          control={control}
-          name="selectGender"
-          placeholder="select Gender"
-          selectList={selectList}
-        />
-      </div>
-      <ImageUpload
-        onImageChange={handleImageChange}
-        defaultImageUrl="https://via.placeholder.com/100"
-      />
     </div>
   );
 }
