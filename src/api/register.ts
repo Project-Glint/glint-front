4d449741a2523931f0e047ff299a78b07dd87196
Normal-case <8282chan94@gmail.com>
2024-11-13 01:36:50 +0900

    feat: react-query mutation 예제 적용

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,3 +1,4 @@
+import { CommonResp, CompanyInfo } from 'types/api/register';
 import { httpClient } from './axios';
 
 /**
@@ -5,8 +6,22 @@ import { httpClient } from './axios';
  * @request GET:/api/v1/company/name
  */
 export async function searchCompanyName(companyName: string) {
-  const response = await httpClient.get(`/api/v1/company/name`, {
-    params: { companyName },
-  });
+  const response = await httpClient.get<CommonResp<CompanyInfo[]>>(
+    `/api/v1/company/name`,
+    {
+      params: { companyName },
+    }
+  );
+  return response.data;
+}
+
+/**
+ * @summary 기업명 id 검색
+ * @request GET:/api/v1/company/:id
+ */
+export async function searchCompanyId(companyId: number) {
+  const response = await httpClient.get<CommonResp<CompanyInfo>>(
+    `/api/v1/company/${companyId}`
+  );
   return response.data;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,75 @@
 'use client';
 
 import { useState } from 'react';
-import { searchCompanyName } from 'api/register';
+import {
+  useSearchCompanyName,
+  useSearchCompanyId,
+} from 'hooks/queries/register';
+import { CompanyInfo } from 'types/api/register';
+
+interface Result {
+  companyNameResult: CompanyInfo[];
+  companyIdResult: Partial<CompanyInfo>;
+}
 
 /**
  * App HOME
  * @return {JSX}
  */
 export default function Home() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState({
+    name: '',
+    id: 0,
+  });
+  const [result, setResult] = useState<Result>({
+    companyNameResult: [],
+    companyIdResult: {},
+  });
+  const { mutate: searchCompanyId } = useSearchCompanyId();
 
-  const clickButton = () => {
-    const data = searchCompanyName(value);
-    console.log(data);
+  const { mutate: searchCompanyName } = useSearchCompanyName();
+
+  const clickButton = (type: string) => {
+    if (type === 'company') {
+      searchCompanyName(value.name, {
+        onSuccess(response) {
+          setResult((prev) => ({ ...prev, companyNameResult: response.data }));
+        },
+      });
+      return;
+    }
+    searchCompanyId(value.id, {
+      onSuccess(response) {
+        setResult((prev) => ({ ...prev, companyIdResult: response.data }));
+      },
+    });
   };
   return (
     <div>
-      <h1>Home</h1>
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick={clickButton}>검색</button>
+      <h1>회사명 검색</h1>
+      <input
+        value={value.name}
+        onChange={(e) =>
+          setValue((prev) => ({ ...prev, name: e.target.value }))
+        }
+      />
+      <button onClick={() => clickButton('company')}>검색</button>
+      {result.companyNameResult.map((company) => (
+        <span key={company.companyId}>{company.companyName}</span>
+      ))}
+      <h1>회사명 아이디</h1>
+      <input
+        type="number"
+        value={value.id}
+        onChange={(e) =>
+          setValue((prev) => ({
+            ...prev,
+            id: Number(e.target.value),
+          }))
+        }
+      />
+      <button onClick={() => clickButton('id')}>검색</button>
+      <span>{result.companyIdResult.companyName}</span>
     </div>
   );
 }
