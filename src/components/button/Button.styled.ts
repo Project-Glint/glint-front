493dd09ebeb3cf63f7ad8f2a28aae728f1cd7d97
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-12 20:26:22 +0900

    feat: button color style 예시코드작성 (#36)

--- src/components/button/Button.styled.ts
+++ src/components/button/Button.styled.ts
@@ -4,14 +4,23 @@ import { buttonVariant } from 'types';
 
 // TODO: 디자인 시스템 나오면 수정
 export const Button = styled.button<{ variant?: buttonVariant }>`
-  ${({ variant }) => css`
+  ${({ variant, theme }) => css`
+    color: ${theme.colors.gray0};
+    background-color: ${theme.colors.default};
     padding: 10px;
     border-radius: 8px;
     cursor: pointer;
-    border: 1px solid #000;
 
     &:disabled {
-      color: #cccccc;
+      background-color: ${theme.colors.disabled};
+    }
+
+    &:hover {
+      background-color: ${theme.colors.hover};
+    }
+
+    &:active {
+      background-color: ${theme.colors.active};
     }
   `}
 `;
