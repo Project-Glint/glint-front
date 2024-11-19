8c8d47ed3f1a4c5daee3185885ac8ad1c7dc8d4c
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-20 00:19:03 +0900

    feat: button css props 추가 (#43)

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,10 +1,12 @@
 // 'use client';
 import { buttonVariant } from 'types';
 import * as S from './Button.styled';
+import { SerializedStyles } from '@emotion/react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: buttonVariant;
   handleClick?: () => void;
+  css?: SerializedStyles;
 }
 
 const Button = ({ children, variant, handleClick, ...rest }: ButtonProps) => {
