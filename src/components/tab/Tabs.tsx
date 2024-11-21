b1ce678c8519dedc4c3e947563fe82c2c065cd97
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-21 16:58:06 +0900

    feat: Tab onChange추가 (#43)

--- src/components/tab/Tabs.tsx
+++ src/components/tab/Tabs.tsx
@@ -2,10 +2,14 @@ import * as S from './Tabs.styled';
 
 interface TabsProps {
   tabList: readonly { key: string; label: string }[];
+  onChange?: (key: string) => void;
 }
-const Tabs = ({ tabList }: TabsProps) => {
+const Tabs = ({ tabList, onChange }: TabsProps) => {
   return (
-    <S.TabsRoot defaultValue={tabList[0].key}>
+    <S.TabsRoot
+      defaultValue={tabList[0].key}
+      onValueChange={(value) => onChange?.(value)}
+    >
       <S.TabsList>
         {tabList.map((tab) => (
           <S.TabsTrigger key={tab.key} value={tab.key}>
