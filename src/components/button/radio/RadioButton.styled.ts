957715aac43187172d20aa29d516ff2d222158e2
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-21 19:24:33 +0900

    style: radiobutton css 수정 (#43)

--- src/components/button/radio/RadioButton.styled.ts
+++ src/components/button/radio/RadioButton.styled.ts
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, SerializedStyles } from '@emotion/react';
 import styled from '@emotion/styled';
 import {
   RadioGroup,
@@ -6,17 +6,24 @@ import {
   RadioGroupItem,
 } from '@radix-ui/react-radio-group';
 
-export const RadioRoot = styled(RadioGroup)`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+export const RadioRoot = styled(RadioGroup)<{ css?: SerializedStyles }>`
+  ${({ css: customCss }) => css`
+    display: flex;
+    flex-direction: column;
+    ${customCss}
+  `}
 `;
 
 export const RadioWrapper = styled.div`
   display: flex;
   align-items: center;
 `;
-
+export const RadioItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+`;
 export const RadioItem = styled(RadioGroupItem)`
   ${({ theme }) => css`
     width: 20px;
@@ -25,6 +32,7 @@ export const RadioItem = styled(RadioGroupItem)`
     border: 1px solid ${theme.colors.gray40};
     background-color: ${theme.colors.gray0};
     display: flex;
+
     align-items: center;
     justify-content: center;
 
@@ -50,6 +58,7 @@ export const RadioIndicator = styled(RadioGroupIndicator)`
 `;
 
 export const Label = styled.label`
-  font-size: 20px;
-  padding-left: 15px;
+  ${({ theme }) => css`
+    ${theme.fonts.body_16_M};
+  `}
 `;
