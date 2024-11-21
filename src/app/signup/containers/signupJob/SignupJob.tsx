76f6e808ac0d61af82789865a7f0c1feeba05236
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-21 17:01:17 +0900

    feat: signupJob 수정 (#43)

--- src/app/signup/containers/signupJob/SignupJob.tsx
+++ src/app/signup/containers/signupJob/SignupJob.tsx
@@ -1,36 +1,53 @@
 import { jobTabList } from 'assets';
 import { Tabs, TextController } from 'components';
 import * as S from './SignupJob.styled';
-import { Control, UseFormSetValue, UseFormWatch } from 'react-hook-form';
+import { Control, UseFormWatch } from 'react-hook-form';
 import { SignupForm } from 'types';
+import { useState } from 'react';
 
 interface SignupJobProps {
   control: Control<any>;
-  setValue: UseFormSetValue<SignupForm>;
   watch: UseFormWatch<SignupForm>;
 }
 
-const SignupJob = ({ control, setValue, watch }: SignupJobProps) => {
+const SignupJob = ({ control, watch }: SignupJobProps) => {
+  // TODO: 전역관리로 수정
+  const [activeTab, setActiveTab] = useState<string>(jobTabList[0].key);
+
+  const handleTabChange = (key: string) => {
+    setActiveTab(key);
+  };
+
+  const renderContent = (worker: string, student: string) => {
+    return activeTab === 'worker' ? worker : student;
+  };
   return (
     <>
-      <Tabs tabList={jobTabList} />
+      <Tabs tabList={jobTabList} onChange={handleTabChange} />
       <S.InputWrapper>
-        <S.InputLabel>직장</S.InputLabel>
+        <S.InputLabel>{renderContent('직장', '학교')}</S.InputLabel>
         <TextController
-          name="companyName"
+          name={renderContent('companyName', 'university')}
           control={control}
-          value={watch('companyName')}
+          value={renderContent(watch('companyName'), watch('university'))}
           rules={{ required: true, minLength: 2, maxLength: 15 }}
-          placeholder="삼성전자, 현대 자동차, 네이버 등"
+          placeholder={renderContent(
+            '삼성전자, 현대 자동차, 네이버 등',
+            '한국대학교, 가나다대학교 등'
+          )}
         />
       </S.InputWrapper>
       <S.InputWrapper>
-        <S.InputLabel>직업</S.InputLabel>
+        <S.InputLabel>{renderContent('직업', '학과')}</S.InputLabel>
         <TextController
-          name="job"
+          name={renderContent('job', 'major')}
           control={control}
+          value={renderContent(watch('job'), watch('major'))}
           rules={{ required: true, minLength: 2, maxLength: 15 }}
-          placeholder="의사, 개발자, 공무원, 은행원 등"
+          placeholder={renderContent(
+            '의사, 개발자, 공무원, 은행원 등',
+            '경영학과, 컴퓨터공학과 등'
+          )}
         />
       </S.InputWrapper>
     </>
