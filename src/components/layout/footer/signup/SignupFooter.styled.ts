c54d580baf8e2b9390e07291ef7842465df49adb
Normal-case <8282chan94@gmail.com>
2024-11-28 00:58:09 +0900

    fix: 바텀 footer 너비 화면 비율에 따른 조정

--- src/components/layout/footer/signup/SignupFooter.styled.ts
+++ src/components/layout/footer/signup/SignupFooter.styled.ts
@@ -1,27 +1,28 @@
 import styled from '@emotion/styled';
 export const Footer = styled.div`
-  width: 100%;
+  width: calc(100% - 32px);
   height: 64px;
   position: fixed;
   bottom: 0;
+  left: 50%;
   justify-content: center;
   /* 기본 모바일 스타일 */
-  max-width: 480px;
-  padding: 0 16px;
+  max-width: calc(480px - 32px);
+  transform: translateX(-50%);
 
   /* 태블릿 크기 이상일 때 */
   @media (min-width: 768px) {
-    max-width: 720px;
+    max-width: calc(720px - 32px);
   }
 
   /* 데스크탑 크기 이상일 때 */
   @media (min-width: 1024px) {
-    max-width: 960px;
+    max-width: calc(960px - 32px);
   }
 
   /* 대형 데스크탑 크기 이상일 때 */
   @media (min-width: 1440px) {
-    max-width: 1200px;
+    max-width: calc(1200px - 32px);
   }
 `;
 
