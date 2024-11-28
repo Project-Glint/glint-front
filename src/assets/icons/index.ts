bf2b1ab0afa2fae165c4ef9fe935da435e132ea5
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-28 16:58:33 +0900

    style: +, X icon 추가 (#48)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,5 @@ export { default as AddFileIcon } from './ic_addFile.svg';
 export { default as FemaleIcon } from './ic_female.svg';
 export { default as MaleIcon } from './ic_male.svg';
 export { default as CancelIcon } from './ic_xcircle.svg';
+export { default as PlusIcon } from './ic_plus.svg';
+export { default as WhiteXIcon } from './ic_whiteX.svg';
