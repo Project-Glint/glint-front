86f1fcf2c12dc1edee8c83ef8306bc86eb2eda64
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-11-29 22:08:42 +0900

    style: black X, handlebar icon 추가 (#48)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { default as MaleIcon } from './ic_male.svg';
 export { default as CancelIcon } from './ic_xcircle.svg';
 export { default as PlusIcon } from './ic_plus.svg';
 export { default as WhiteXIcon } from './ic_whiteX.svg';
+export { default as BlackXIcon } from './ic_blackX.svg';
+export { default as HandlebarIcon } from './ic_handlebar.svg';
