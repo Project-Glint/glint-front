3aff8756a66887ea93bac8b675aa1561e0bf80c3
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-12-08 23:32:24 +0900

    feat: 주석제거, 스크롤시 footer에 border추가, footer css 수정 (#56)

--- src/components/layout/footer/signup/SignupFooter.styled.ts
+++ src/components/layout/footer/signup/SignupFooter.styled.ts
@@ -1,33 +1,37 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-export const Footer = styled.div`
-  ${({ theme }) => css`
-    width: calc(100% - 32px);
+export const Footer = styled.div<{ hasScrolled: boolean }>`
+  ${({ theme, hasScrolled }) => css`
+    width: 100%;
     height: 80px;
     position: fixed;
     bottom: 0;
-    left: 50%;
+    left: 0;
+    right: 0;
+    padding: 0 16px;
     justify-content: center;
     align-content: center;
     // TODO: 디자인 시스템 확인
-    // background-color: ${theme.colors.gray0};
+    background-color: ${theme.colors.gray0};
+    border-top: ${hasScrolled && `1px solid ${theme.colors.gray30}`};
+
     /* 기본 모바일 스타일 */
-    max-width: calc(480px - 32px);
-    transform: translateX(-50%);
+    max-width: 480px;
+    margin: 0 auto;
 
     /* 태블릿 크기 이상일 때 */
     @media (min-width: 768px) {
-      max-width: calc(720px - 32px);
+      max-width: 720px;
     }
 
     /* 데스크탑 크기 이상일 때 */
     @media (min-width: 1024px) {
-      max-width: calc(960px - 32px);
+      max-width: 960px;
     }
 
     /* 대형 데스크탑 크기 이상일 때 */
     @media (min-width: 1440px) {
-      max-width: calc(1200px - 32px);
+      max-width: 1200px;
     }
   `}
 `;
