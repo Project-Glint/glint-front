38b1d1cda42e2ff7024ab28410219c1fdde4bc7c
Narae Lee <81891997+dlskfo306@users.noreply.github.com>
2024-12-08 15:06:38 +0900

    feat: Chip컴포넌트 추가, 기존 Badge => Chip으로 수정 (#54)

---
+++ src/components/chip/Chip.styled.ts
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const ChipWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+`;
+
+export const Chip = styled.button<{
+  isSelected?: boolean;
+  icon?: boolean;
+  size: 'sm' | 'md' | 'lg';
+}>`
+  ${({ isSelected, icon, theme, size }) => css`
+    ${size === 'lg' ? theme.fonts.body_14_M : theme.fonts.body_12_M}
+    display: flex;
+    align-items: center;
+    gap: ${icon ? '4px' : 0};
+    padding: ${size === 'lg'
+      ? '8px 12px'
+      : size === 'md'
+        ? '4px 12px'
+        : '4px 8px'};
+    border-radius: 19px;
+    cursor: ${icon ? 'default' : 'pointer'};
+    height: ${size === 'lg' ? '40px' : size === 'md' ? '32px' : '24px'};
+    width: fit-content;
+    border: ${!isSelected
+      ? `1px solid ${theme.colors.gray30}`
+      : `1px solid ${theme.colors.primary60}`};
+    color: ${isSelected ? theme.colors.gray0 : theme.colors.gray90};
+    background-color: ${isSelected && theme.colors.primary60};
+
+    svg {
+      overflow: visible;
+      width: 16px;
+      height: 16px;
+      & path {
+        fill: ${isSelected && theme.colors.gray0};
+      }
+    }
+  `};
+`;
+
+export const Icon = styled.span`
+  cursor: pointer;
+`;
---
+++ src/components/chip/Chip.tsx
@@ -0,0 +1,64 @@
+import * as S from './Chip.styled';
+
+interface ChipItem {
+  key: string;
+  label: string;
+}
+
+interface ChipProps {
+  className?: string;
+  items: ChipItem | ChipItem[];
+  selectedKeys?: string[];
+  handleClick: (key: string) => void;
+  size: 'sm' | 'md' | 'lg';
+  icon?: React.ReactNode;
+}
+
+const Chip = ({
+  className,
+  items,
+  selectedKeys,
+  handleClick,
+  size,
+  icon,
+}: ChipProps) => {
+  const renderChip = (item: ChipItem) => (
+    <S.Chip
+      className={className}
+      key={item.key}
+      isSelected={selectedKeys?.includes(item.key)}
+      icon={!!icon}
+      size={size}
+      onClick={!icon ? () => handleClick(item.key) : undefined}
+      type="button"
+    >
+      {item.label}
+      {icon && (
+        // <span
+        //   onClick={(e) => {
+        //     e.stopPropagation();
+        //     handleClick(item.key);
+        //   }}
+        //   style={{ cursor: 'pointer' }}
+        // >
+        //   {icon}
+        // </span>
+        <S.Icon
+          onClick={(e) => {
+            e.stopPropagation();
+            handleClick(item.key);
+          }}
+        >
+          {icon}
+        </S.Icon>
+      )}
+    </S.Chip>
+  );
+  return (
+    <S.ChipWrapper>
+      {Array.isArray(items) ? items.map(renderChip) : renderChip(items)}
+    </S.ChipWrapper>
+  );
+};
+
+export default Chip;
